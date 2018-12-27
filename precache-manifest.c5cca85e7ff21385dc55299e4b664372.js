23711e7d67a47b0bdce61045ac31c058cf0be930
Victor Freitas <victorfreitas1@gmail.com>
2018-12-27 01:23:30 -0200

    Updates

---
+++ precache-manifest.c5cca85e7ff21385dc55299e4b664372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/static/media/RobotoMono-Thin.b93c2ad4.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e20d65eaac122db66dba",
+    "url": "/static/js/main.e20d65ea.chunk.js"
+  },
+  {
+    "revision": "e6b2b04c5de1670f9e53",
+    "url": "/static/js/1.e6b2b04c.chunk.js"
+  },
+  {
+    "revision": "e20d65eaac122db66dba",
+    "url": "/static/css/main.51732830.chunk.css"
+  },
+  {
+    "revision": "9d30708ea78235895e1651d2b8c7cfb8",
+    "url": "/index.html"
+  }
+];
